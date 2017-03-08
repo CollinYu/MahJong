8a66a701bf3cd3043ae7fb6ff65742c2accf0e77
TristanBoylan <boylan19@up.edu>
2017-03-08 15:40:29 -0800

    Testing out frame Layout in the GUI also added a deck class and some basic instance variables

---
+++ app/src/main/java/edu/up/yu18/mahjong/Deck.java
@@ -0,0 +1,21 @@
+package edu.up.yu18.mahjong;
+
+/**
+ * Created by boylan19 on 3/8/2017.
+ */
+public class Deck {
+
+    private int val;
+    private boolean immage;
+    private int suit;
+    private int curNum;
+    private int location;
+
+    private Deck(){
+
+    }
+
+
+
+
+}
