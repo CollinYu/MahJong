3c004c600d23868197a62eddad249ab2cdb697a7
TristanBoylan <boylan19@up.edu>
2017-04-12 17:46:17 -0700

    edited deck and tile

--- app/src/main/java/edu/up/yu18/mahjong/game/Deck.java
+++ app/src/main/java/edu/up/yu18/mahjong/game/Deck.java
@@ -1,5 +1,7 @@
 package edu.up.yu18.mahjong.game;
 
+import java.util.ArrayList;
+
 /**
  * Created by boylan19 on 3/8/2017.
  */
@@ -8,12 +10,28 @@ public class Deck {
     private int curNum;
     private boolean image;
     private int location;
+    private ArrayList<Tile> deckTiles;
+    private String suit;
 
     private Deck(){
+        deckTiles = new ArrayList<Tile>();
+        int i = 0;
+        for(int j = 0; j < 4; j++){
+            for(int k = 0; k < 3; k++){
+                for(int l = 0; l < 9; l++) {
+                    deckTiles.add(new Tile(k,l,i));
+                    i++;
+                }//l
+            }//k
+        }//j
+        for (int j = 0; j < 4; j++) {
+            deckTiles.add(new Tile(4, 1, i));
+            i++;//4 is dragon
+        }
+        for (int j = 0; j < 4; j++) {
+            deckTiles.add(new Tile(5, 1, i));
+            i++;//5 is wind
+        }
 
     }
-
-
-
-
 }
