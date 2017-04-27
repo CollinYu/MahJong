615641c2281fe7f71f2db3c6d8b44119b32430d6
CollinYu <yu18@up.edu>
2017-04-26 21:05:26 -0700

    empty

--- app/src/main/java/edu/up/yu18/mahjong/game/MahJong/game/MahJongSmartPlayer.java
+++ app/src/main/java/edu/up/yu18/mahjong/game/MahJong/game/MahJongSmartPlayer.java
@@ -59,74 +59,79 @@ public class MahJongSmartPlayer extends GameComputerPlayer {
             }
             isFirstTurn = false;
         }
-
-        if(state.getGameStage()%2 == 0 && !state.hasPassed(this.playerNum))
-        {
-            GameAction action = new Pass(this,playerNum);
-            for (int i =0;i < state.getPlayerClosedHandLength(playerNum)-1;i++){
-                if(state.getPlayerClosedHandTile(playerNum,i).isEqualTo(state.getCurrDiscard())) {
-                    if ((state.getPlayerClosedHandTile(playerNum, i + 1)) != null) {
-                        if (state.getPlayerClosedHandTile(playerNum, i).isEqualTo(state.getPlayerClosedHandTile(playerNum, i + 1))) {
-                            if (state.getPlayerClosedHandTile(playerNum, i + 2) != null) {
-                                if (state.getPlayerClosedHandTile(playerNum,i).isEqualTo(state.getPlayerClosedHandTile(playerNum,i+2))){
-                                    action = new Kong(this,playerNum,state.getPlayerClosedHandTile(playerNum,i),state.getPlayerClosedHandTile(playerNum,i+1),state.getPlayerClosedHandTile(playerNum,i+2),state.getCurrDiscard());
+        if(state.getGameStage()%2 == 0 && !state.hasPassed(this.playerNum)) {
+            GameAction action = new Pass(this, playerNum);
+            if(state.getCurrDiscard() != null) {
+                for (int j = 0; j < state.getPlayerClosedHandLength(playerNum) - 1; j++) {
+                    if (state.getPlayerClosedHandTile(playerNum, j).isEqualTo(state.getCurrDiscard())) {
+                        if ((state.getPlayerClosedHandTile(playerNum, j + 1)) != null) {
+                            if (state.getPlayerClosedHandTile(playerNum, j).isEqualTo(state.getPlayerClosedHandTile(playerNum, j + 1))) {
+                                if (state.getPlayerClosedHandTile(playerNum, j + 2) != null) {
+                                    if (state.getPlayerClosedHandTile(playerNum, j).isEqualTo(state.getPlayerClosedHandTile(playerNum, j + 2))) {
+                                        action = new Kong(this, playerNum, state.getPlayerClosedHandTile(playerNum, j), state.getPlayerClosedHandTile(playerNum, j + 1), state.getPlayerClosedHandTile(playerNum, j + 2), state.getCurrDiscard());
+                                    }
+                                } else {
+                                    action = new Pong(this, playerNum, state.getPlayerClosedHandTile(playerNum, j), state.getPlayerClosedHandTile(playerNum, j + 1), state.getCurrDiscard());
                                 }
                             }
-                            else {
-                                action = new Pong(this, playerNum, state.getPlayerClosedHandTile(playerNum, i), state.getPlayerClosedHandTile(playerNum, i + 1), state.getCurrDiscard());
+
+                        }
+                    }
+                }
+
+                int[] hand;
+                Tile curPlay = state.getCurrDiscard();
+                hand = state.getWholeClosedHand(this.playerNum);
+                if (state.getGameStage() % 2 == 0) {
+                    hand[hand.length - 1] = curPlay.getDeckPos();
+                }
+                Arrays.sort(hand);
+                //CHOWWWWW
+                for (int i = 0; i < hand.length - 2; i++) {
+                    if (state.getPlayerClosedHandTile(this.playerNum, i).isEqualTo(curPlay)) {
+                        if (state.getPlayerClosedHandTile(this.playerNum, i + 1).isAbove(curPlay)
+                                && state.getPlayerClosedHandTile(this.playerNum, i + 2).isAbove(state.getPlayerClosedHandTile(this.playerNum, i + 1))) {
+                            action = new Chow(this, playerNum, state.getPlayerClosedHandTile(this.playerNum, i + 1)
+                                    , state.getPlayerClosedHandTile(this.playerNum, i + 2), curPlay);
+                        }
+                        if (i > 0) {
+                            if (state.getPlayerClosedHandTile(this.playerNum, i + 1).isAbove(curPlay)
+                                    && state.getPlayerClosedHandTile(this.playerNum, i - 1).isBelow(curPlay)) {
+                                action = new Chow(this, playerNum, state.getPlayerClosedHandTile(this.playerNum, i + 1)
+                                        , state.getPlayerClosedHandTile(this.playerNum, i - 1), curPlay);
+
+                            }
+                        }
+                        if (i > 1) {
+                            if (state.getPlayerClosedHandTile(this.playerNum, i - 1).isBelow(curPlay)
+                                    && state.getPlayerClosedHandTile(this.playerNum, i - 2).isBelow(curPlay)) {
+                                action = new Chow(this, playerNum, state.getPlayerClosedHandTile(this.playerNum, i - 1)
+                                        , state.getPlayerClosedHandTile(this.playerNum, i - 2), curPlay);
                             }
                         }
 
+                        break;
+                    }
+                }
+
+
+                if (action instanceof Pass) {
+                    for (int i = 0; i < state.getPlayerClosedHandLength(playerNum); i++) {
+                        if (state.getCurrDiscard().isEqualTo(state.getPlayerClosedHandTile(playerNum, i))) {
+                            hand[i] -= 2;
+                        }
+                        if (state.getCurrDiscard().isAbove(state.getPlayerClosedHandTile(playerNum, i))) {
+                            hand[i] -= 2;
+                        }
+                        if (state.getCurrDiscard().isBelow(state.getPlayerClosedHandTile(playerNum, i))) {
+                            hand[i] -= 2;
+                        }
                     }
                 }
             }
+                //checks if the current discard lowers the value of any of the cards in its hand
+                game.sendAction(action);
 
-            int[] hand;
-            Tile curPlay = state.getCurrDiscard();
-            hand = state.getWholeClosedHand(this.playerNum);
-            if(state.getGameStage() % 2 == 0){hand[hand.length-1] = curPlay.getDeckPos();}
-            Arrays.sort(hand);
-            //CHOWWWWW
-            for (int i = 0; i < hand.length;i++){
-                if(state.getPlayerClosedHandTile(this.playerNum, hand[i]).isEqualTo(curPlay)){
-                    if (state.getPlayerClosedHandTile(this.playerNum, hand[i+1]).isAbove(curPlay)
-                            &&state.getPlayerClosedHandTile(this.playerNum, hand[i+2]).isAbove(curPlay)){
-                        action = new Chow(this,playerNum,state.getPlayerClosedHandTile(this.playerNum,i+1)
-                                ,state.getPlayerClosedHandTile(this.playerNum,i+2),curPlay);
-                    }
-                    if (state.getPlayerClosedHandTile(this.playerNum, hand[i+1]).isAbove(curPlay)
-                            &&state.getPlayerClosedHandTile(this.playerNum, hand[i-1]).isBelow(curPlay)){
-                        action = new Chow(this,playerNum,state.getPlayerClosedHandTile(this.playerNum,i+1)
-                                ,state.getPlayerClosedHandTile(this.playerNum,i-1),curPlay);
-
-                    }
-                    if (state.getPlayerClosedHandTile(this.playerNum, hand[i-1]).isBelow(curPlay)
-                            &&state.getPlayerClosedHandTile(this.playerNum, hand[i-2]).isBelow(curPlay)){
-                        action = new Chow(this,playerNum,state.getPlayerClosedHandTile(this.playerNum,i-1)
-                                ,state.getPlayerClosedHandTile(this.playerNum,i-2),curPlay);
-                    }
-
-                    break;
-                }
-            }
-
-
-
-            if(action instanceof Pass) {
-                for (int i = 0; i < state.getPlayerClosedHandLength(playerNum); i++) {
-                    if (state.getCurrDiscard().isEqualTo(state.getPlayerClosedHandTile(playerNum, i))) {
-                        hand[i] -= 2;
-                    }
-                    if (state.getCurrDiscard().isAbove(state.getPlayerClosedHandTile(playerNum, i))) {
-                        hand[i] -= 2;
-                    }
-                    if (state.getCurrDiscard().isBelow(state.getPlayerClosedHandTile(playerNum, i))) {
-                        hand[i] -= 2;
-                    }
-                }
-            }
-            //checks if the current discard lowers the value of any of the cards in its hand
-            game.sendAction(action);
         }
 
         // if it's this' turn, discards the first card in its hand
